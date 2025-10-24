409e81dce8a0ed4de920be4a5a2df65f870e3377
ordin <ordinarystory@yandex.ru>
2025-10-24 17:09:19 +0300

    php artisan storage:link php artisan make:middleware GoogleRecaptcha php artisan make:controller IndexController php artisan make:controller PostController php artisan make:controller AuthController php artisan make:request CommentForm php artisan make:request ContactFormRequest php artisan make:mail ContactForm php artisan make:mail ForgotPassword requests: Forgot, Login, Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\ForgotRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use App\Mail\ForgotPassword;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -19,41 +21,31 @@ class AuthController extends Controller
     public function showRegisterForm() {
         return view('auth.register');
     }
-    public function register(Request $request) {
-        $data = $request->validate([
-            'name' => 'required|string',
-           'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
+    public function register(RegisterRequest $request) {
+        $validated = $request->validated();
         $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password'])
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password'])
         ]);
         if ($user) {
             auth('web')->login($user);
         }
         return redirect(route('home'));
     }
-    public function login(Request $request) {
-        $data = $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-        ]);
-
-       if (auth('web')->attempt($data)) {
+    public function login(LoginRequest $request) {
+       $validated = $request->validated();
+       if (auth('web')->attempt($validated)) {
            return redirect(route('home'));
        }
-        return redirect(route('login'))->withErrors(['email' => 'Пользователь не найден']);
+       return redirect(route('login'))->withErrors(['email' => 'Пользователь не найден']);
     }
     public function showForgotForm() {
         return view('auth.forgot');
     }
-    public function forgot(Request $request) {
-        $data = $request->validate([
-            'email' => 'required|string|email|exists:users',
-        ]);
-        $user = User::where(['email' => $data['email']])->first();
+    public function forgot(ForgotRequest $request) {
+        $validated = $request->validated();
+        $user = User::where(['email' => $validated['email']])->first();
         $password = uniqid();
         $user->password = bcrypt($password);
         $user->save();
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,15 +12,12 @@ class IndexController extends Controller
 {
     public function index() {
         $post = Post::orderBy('created_at', 'desc')->limit(3)->get();
-
         return view('welcome', ['posts' => $post]);
     }
     public function showContactForm() {
-
         return view('contact_form');
     }
     public function contactForm(ContactFormRequest $request) {
-
         Mail::to('ordinarystory@yandex.ru')->send(new ContactForm($request->validated()));
         return redirect('/contacts');
     }
